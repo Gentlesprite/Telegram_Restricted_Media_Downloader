556892be769747ed54853ef686e8ae0090bfb934
Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>
2026-03-19 01:28:51 +0800

    v1.9.6 重构build.py常量定义。 v1.9.6 添加构建时的版本检查。
    Signed-off-by: Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>

--- build.py
+++ build.py
@@ -14,15 +14,19 @@ from module import AUTHOR, __version__, __update_date__, SOFTWARE_SHORT_NAME
 from module.ttyd import TTYD
 from module.tmux import TMUX
 
-platform: str = sys.platform
-uv: str = 'uv ' if which('uv') and os.path.exists('uv.lock') else ''
+VERSION_INFO = sys.version_info
+PLATFORM: str = sys.platform
+UV: str = 'uv ' if which('uv') and os.path.exists('uv.lock') else ''
+TERMINAL_COLUMNS: int = os.get_terminal_size().columns
+GRID_CONTENT: str = '='
+GRID: str = GRID_CONTENT * TERMINAL_COLUMNS
 
 
 def ready_zstandard():
     try:
         import zstandard
     except (ImportError, ModuleNotFoundError, NameError):
-        subprocess.run(f'{uv}pip install zstandard', shell=True)
+        subprocess.run(f'{UV}pip install zstandard', shell=True)
 
 
 def ready_nuitka():
@@ -30,9 +34,9 @@ def ready_nuitka():
         import nuitka
     except (ImportError, ModuleNotFoundError, NameError):
         if sys.version_info >= (3, 13):
-            subprocess.run(f'{uv}pip install nuitka==2.6.7', shell=True)
+            subprocess.run(f'{UV}pip install nuitka==2.6.7', shell=True)
         else:
-            subprocess.run(f'{uv}pip install nuitka', shell=True)
+            subprocess.run(f'{UV}pip install nuitka', shell=True)
 
 
 def ready_pymediainfo() -> tuple:
@@ -40,7 +44,7 @@ def ready_pymediainfo() -> tuple:
         import pymediainfo
         mediainfo_lib_meta = None
         mediainfo_lib_directory = os.path.dirname(pymediainfo.__file__)
-        if platform == 'win32':
+        if PLATFORM == 'win32':
             file_name = 'MediaInfo.dll'
             file_path = os.path.join(mediainfo_lib_directory, file_name)
             if os.path.isfile(file_path):
@@ -64,7 +68,7 @@ def ready_pymediainfo() -> tuple:
                     }
         if mediainfo_lib_meta:
             return mediainfo_lib_meta.get('file_name'), mediainfo_lib_meta.get('file_path')
-        file_name = 'MediaInfo.dll' if platform == 'win32' else 'libmediainfo.so.0'
+        file_name = 'MediaInfo.dll' if PLATFORM == 'win32' else 'libmediainfo.so.0'
         path = str(Path(f'res/bin/{file_name}').resolve())
         if os.path.isfile(path):
             return file_name, path
@@ -72,7 +76,7 @@ def ready_pymediainfo() -> tuple:
         sys.exit(1)
     except (ImportError, ModuleNotFoundError, NameError):
         if sys.version_info >= (3, 9):
-            subprocess.run(f'{uv}pip install pymediainfo==7.0.1', shell=True)
+            subprocess.run(f'{UV}pip install pymediainfo==7.0.1', shell=True)
             print(f'缺少pymediainfo依赖已自动安装,正在重启...')
             subprocess.run([sys.executable] + sys.argv)
         else:
@@ -99,18 +103,41 @@ def ready_tmux():
 
 
 def build(command):
-    print(command)
+    print(f'Command:\n{command}\n{GRID}')
+    print('Build in progress:')
     subprocess.run(command, shell=True)
 
 
+def check_python_version():
+    """检查Python版本是否满足：3.9.0 ≤ Python版本 < 3.14.0"""
+
+    current_version = (VERSION_INFO.major, VERSION_INFO.minor, VERSION_INFO.micro)
+
+    min_version = (3, 9, 0)
+    max_version = (3, 14, 0)
+
+    version_valid = (
+            VERSION_INFO.major == 3
+            and min_version <= current_version < max_version
+    )
+
+    if not version_valid:
+        print(
+            f'Python版本不满足要求\n当前版本:{sys.version}\n要求范围:3.9.0 ≤ Python 版本 < 3.14.0\n请安装符合要求的Python版本后重试。')
+        sys.exit(1)
+
+    print(f'{GRID}\nPython:\n{sys.version}\n{GRID}')
+
+
 if __name__ == '__main__':
+    check_python_version()
     try:
         ready_nuitka()
         ready_zstandard()
         media_info_lib_filename, media_info_lib_path = ready_pymediainfo()
         ttyd_filename, ttyd_path = ready_ttyd()
         tmux_filename, tmux_path = ready_tmux()
-        extension = '.exe' if platform == 'win32' else ''
+        extension = '.exe' if PLATFORM == 'win32' else ''
         ico_path = 'res/icon.ico'
         output = 'output'
         main = 'main.py'
