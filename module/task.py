9226cd98557cdbad7970066a6630a2d3f9d28b1c
Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>
2026-09-06 22:40:06 +0800

    v2.0.1 新增下载完成的日志。
    Signed-off-by: Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>

--- module/task.py
+++ module/task.py
@@ -106,6 +106,7 @@ class DownloadTask:
                 DownloadTask.LINK_INFO.get(link)['error_msg'] = {}
                 DownloadTask.COMPLETE_LINK.add(link)
                 asyncio.create_task(self.done_notice(f'"{link}"下载完成。'))
+                log.info(f'链接:"{link}"下载完成。')
             return res
 
         return wrapper
