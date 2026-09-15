f826eb8eaab65deadc6cbf0f042f29fd0b845d48
Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>
2026-09-16 05:16:28 +0800

    v2.0.3 更新LICENCE文件。 v2.0.3 将pyproject.toml版本号升级到2.0.3。 v2.0.3 更新README.md文件。 v2.0.3 更新使用必读.pdf文件。
    Signed-off-by: Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>

--- module/__init__.py
+++ module/__init__.py
@@ -76,8 +76,8 @@ console = Console(log_path=False, log_time_format=LOG_TIME_FORMAT)
 SLEEP_THRESHOLD = 60
 AUTHOR = 'Gentlesprite'
 __version__ = '2.0.3'
-__license__ = 'MIT License'
-__update_date__ = '2026/09/15 17:16:04'
+__license__ = 'GPL-3.0-or-later'
+__update_date__ = '2026/09/16 05:15:05'
 __copyright__ = f'Copyright (C) 2024-{__update_date__[:4]} {AUTHOR} <https://github.com/Gentlesprite>'
 SOFTWARE_FULL_NAME = 'Telegram Restricted Media Downloader'
 SOFTWARE_SHORT_NAME = 'TRMD'
