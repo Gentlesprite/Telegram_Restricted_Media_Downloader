82fd20de763b5bd911d4eb1ce242dd8740e7ed9d
Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>
2026-09-26 01:35:03 +0800

    v2.0.8 使用zig编译替代msvc以修复Windows发布时出现C1002的问题。 v2.0.8 移除ci中Windows发布构建时的--low-memory参数。
    Signed-off-by: Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>

--- build.py
+++ build.py
@@ -90,8 +90,9 @@ if __name__ == '__main__':
         copy_right = f'Copyright (C) 2024-{years} {AUTHOR}.All rights reserved.'
         build_command = f'{sys.executable} -m '
         build_command += f'nuitka --standalone --onefile '
+        build_command += f'--zig '
         build_command += f'--no-deployment-flag=self-execution '
-        build_command += f'--msvc=latest --windows-icon-from-ico="{ico_path}" --assume-yes-for-downloads ' if PLATFORM == 'win32' else ''
+        build_command += f'--windows-icon-from-ico="{ico_path}" --assume-yes-for-downloads ' if PLATFORM == 'win32' else ''
         build_command += f'--include-package-data=pyrogram '
         build_command += f'--include-module=pygments.lexers.data '
         build_command += ''.join(map(lambda d: f'--include-data-dir="{d[0]}"="{d[1]}" ', ready_web()))
